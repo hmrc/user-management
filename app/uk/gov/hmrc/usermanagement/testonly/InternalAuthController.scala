d1ba75e617570fccea8ba7161b2ff52119397f31
macgrewal <7713555+macgrewal@users.noreply.github.com>
2025-03-18 18:28:32 +0000

    Added dummy internal-auth endpoint for local development

---
+++ app/uk/gov/hmrc/usermanagement/testonly/InternalAuthController.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.usermanagement.testonly
+
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class InternalAuthController @Inject()(
+  cc: ControllerComponents
+)(using
+  ExecutionContext
+) extends BackendController(cc):
+
+  def retrieveUmpToken: Action[AnyContent] =
+    Action.async: _ =>
+      Future.successful(Ok(Json.toJson("dummy ump token")))
